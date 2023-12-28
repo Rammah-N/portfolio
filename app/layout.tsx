359cfec55ef6de21ba029e878f32306dc6a8bf77
rammah <ramahnore@gmail.com>
2023-12-28 10:39:35 +0400

    add goatcounter and favicon

--- app/layout.tsx
+++ app/layout.tsx
@@ -17,6 +17,30 @@ export default function RootLayout({
 }) {
 	return (
 		<html lang="en">
+			<head>
+				<script
+					data-goatcounter="https://rammah.goatcounter.com/count"
+					async
+					src="//gc.zgo.at/count.js"></script>
+				<link
+					rel="apple-touch-icon"
+					sizes="180x180"
+					href="/apple-touch-icon.png"
+				/>
+				<link
+					rel="icon"
+					type="image/png"
+					sizes="32x32"
+					href="/favicon-32x32.png"
+				/>
+				<link
+					rel="icon"
+					type="image/png"
+					sizes="16x16"
+					href="/favicon-16x16.png"
+				/>
+				<link rel="manifest" href="/site.webmanifest"></link>
+			</head>
 			<body className={`bg-[#F7F7F8] ${pixel.className}`}>{children}</body>
 		</html>
 	);
